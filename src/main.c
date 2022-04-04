a798a8760a8d14d42e06c4a453b86224670b3be6
Uladzislau <80714867+Uladz1slau@users.noreply.github.com>
2022-04-04 23:21:17 +0000

    add dec2bin function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,44 @@
-/* hello.c */
 #include <stdio.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int main(int argc, const char * argv[]) {
+    int dec2bin(int a);
+    int amount;//кол-во чисел в массиве
+    scanf("%d", &amount);
+    int numbers[amount]; //массив
+    int num; // числа в массиве
+    //int second[amount];
+    //int numSecond;
+    
+    for(int i = 0; i < amount; i++)
+      {
+        scanf("%d", &num);
+          numbers[i] = dec2bin(num);
+          
+      }
+    
+    
+   
+    
+    for(int i = 0; i< amount; i++){ // Запускаем цикл для вывода массива на экран
+        printf("numbers[%d] = %d\n", i, numbers[i]);
+    }
+    
+    return 0;
 }
+
+
+int dec2bin(int a)
+{
+    int bin = 0;
+    int k = 1;
+    
+    while (a)
+    {
+        bin += (a % 2) * k;
+        k = k * 10;
+        a = a / 2;
+    }
+
+    return bin;
+}
+
