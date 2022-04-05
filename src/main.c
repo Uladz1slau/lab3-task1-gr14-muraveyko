304ee6ed3b2b40c98888289237045126541d2aa0
Uladzislau <80714867+Uladz1slau@users.noreply.github.com>
2022-04-05 23:40:40 +0000

    add countOne function

--- src/main.c
+++ src/main.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int summ (int);
+int countOne(int);
 
 int main(int argc, const char * argv[]) {
     int dec2bin(int a);
+    srand(time(NULL));
+
     int amount;//кол-во чисел в массиве
     scanf("%d", &amount);
     int numbers[amount]; //массив
-    int num; // числа в массиве
-    //int second[amount];
-    //int numSecond;
+   // int num; // числа в массиве
+    int i;
     
-    for(int i = 0; i < amount; i++)
-      {
-        scanf("%d", &num);
-          numbers[i] = dec2bin(num);
-          
-      }
-    
-    
-   
-    
-    for(int i = 0; i< amount; i++){ // Запускаем цикл для вывода массива на экран
+    for (i=0; i< amount; i++) {
+        numbers[i] = countOne(dec2bin(rand()%100));
+        
         printf("numbers[%d] = %d\n", i, numbers[i]);
-    }
+        
+        }
+  
     
+
     return 0;
 }
+
+int countOne(int c)
+{
+    int count;
+    for (count = 0; c != 0; c &= (c - 1))
+        ++count;
+    return count;
+    
+}
+
 
 
 int dec2bin(int a)
@@ -41,4 +52,5 @@ int dec2bin(int a)
 
     return bin;
 }
+
 
