2cd81cfc2fb7a2413372b00f008b71e5a69cbb87
Uladzislau <80714867+Uladz1slau@users.noreply.github.com>
2022-04-06 18:06:40 +0000

    add sort function

--- src/main.c
+++ src/main.c
@@ -14,18 +14,40 @@ int main(int argc, const char * argv[]) {
     int numbers[amount]; //массив
    // int num; // числа в массиве
     int i;
+  
+   
     
     for (i=0; i< amount; i++) {
-        numbers[i] = countOne(dec2bin(rand()%100));
-        
+       // numbers[i] = rand()%100;
+       numbers[i] = countOne(dec2bin(rand()%100));
         printf("numbers[%d] = %d\n", i, numbers[i]);
         
         }
-  
     
-
+    for (int i=0; (i < (amount-1)); i++)
+    {
+        for (int m=0; (m < (amount - i - 1)); m++)
+        {
+            if(numbers[m] > numbers[m+1])
+            {
+                int p = numbers[m];
+                numbers[m]=numbers[m + 1];
+                numbers[m + 1] = p;
+            }
+        
+        }
+    
+    }
+    
+    for (i=0; i < amount; i++) {
+        
+    printf("numbers[%d] = %d\n", i, numbers[i]);
+        
+    }
+    
     return 0;
 }
+
 
 int countOne(int c)
 {
@@ -35,8 +57,6 @@ int countOne(int c)
     return count;
     
 }
-
-
 
 int dec2bin(int a)
 {
@@ -52,5 +72,3 @@ int dec2bin(int a)
 
     return bin;
 }
-
-
